e20275787e8f22bfce4456b9a502be3e3ae02ff0
maki0011 <itoga.h.110@gmail.com>
2017-10-20 15:35:32 +0900

    Java日付1

---
+++ CampChallenge_java/015.標準クラス/hiduke1のコピー.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.dbclass;
+
+import java.util.Calendar;
+/**
+ *
+ * @author itoga
+ */
+public class hiduke1 {
+    
+    public static void main(String[] args){
+        
+        Calendar c = Calendar.getInstance();
+        c.set(2016,0,1,0,0,0);
+        System.out.print(c.getTime());
+    
+    
+    
+    }
+}
