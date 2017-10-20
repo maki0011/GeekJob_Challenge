436727f1dd76c701d1086b2a7cbe88b51180fa5f
maki0011 <itoga.h.110@gmail.com>
2017-10-20 17:32:17 +0900

    Java文字列1

---
+++ CampChallenge_java/015.標準クラス/mojiretu1のコピー.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.dbclass;
+
+/**
+ *
+ * @author itoga
+ */
+public class mojiretu1 {
+    public static void main(String[] args){
+        String name = "糸賀";
+        System.out.println(name.length());//2
+        System.out.println(name.getBytes().length);//6
+    
+    }
+}
